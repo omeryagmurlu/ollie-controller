832777acc036911f114f883acfb144c688381631
omeryagmurlu <omeryagmurlu@gmail.com>
2015-12-19 01:02:29 +0200

    v 0.1.1, scripts

--- FIRSTRUN.sh
+++ FIRSTRUN.sh
@@ -2,8 +2,22 @@
 
 npm install
 
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform       
+	echo Mac 
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+	echo Linux
+	wget -O nw.tar.gz http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-linux-x64.tar.gz
+	tar xvzf nw.tar.gz -C src
+	rm nw.tar.gz
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+	echo Win
+    # Do something under Windows NT platform
+fi
+
 (cd node_modules/olliejs/node_modules/noble/node_modules/bluetooth-hci-socket; nw-gyp configure --target=0.12.3; nw-gyp build )
 
-sudo setcap cap_net_raw+eip src/linux/nw
+sudo setcap cap_net_raw+eip src/*/nw
 
-./src/linux/nw .
+./src/*/nw .
